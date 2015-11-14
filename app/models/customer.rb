2bf84eba5e655728a6cb7666e8b9b200644cbd6f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-11-14 12:32:12 +0900

    顧客メールアドレス複数化

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,21 +1,14 @@
 class Customer < ActiveRecord::Base
-  include EmailHolder
   include PersonalNameHolder
   include PasswordHolder
 
+  has_many :emails, dependent: :destroy, autosave: true
   has_many :addresses, dependent: :destroy
   has_one :home_address, autosave: true
   has_one :work_address, autosave: true
   has_many :phones, dependent: :destroy
   has_many :personal_phones, -> { where(address_id: nil).order(:id) },
     class_name: 'Phone', autosave: true
-  has_many :entries, dependent: :destroy
-  has_many :programs, through: :entries
-  has_many :messages
-  has_many :outbound_messages, class_name: 'CustomerMessage',
-    foreign_key: 'customer_id'
-  has_many :inbound_messages, class_name: 'StaffMessage',
-    foreign_key: 'customer_id'
 
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
@@ -29,6 +22,18 @@ class Customer < ActiveRecord::Base
       self.birth_year = birthday.year
       self.birth_month = birthday.month
       self.birth_mday = birthday.mday
+    end
+  end
+
+  def email
+    emails[0].try(:address)
+  end
+
+  def email=(email)
+    if emails.first
+      self.emails[0].address = email
+    else
+      emails.build(address: email)
     end
   end
 end
