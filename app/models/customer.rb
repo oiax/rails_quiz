5bc4bf019429aa23ec379287bd807b6f2c22c9d5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-11-14 21:23:44 +0900

    メールアドレスの大文字・小文字に留意

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,6 +10,30 @@ class Customer < ActiveRecord::Base
   has_many :personal_phones, -> { where(address_id: nil).order(:id) },
     class_name: 'Phone', autosave: true
 
+  attr_writer :email_addresses
+
+  before_validation do
+    if @email_addresses
+      @email_addresses.each_with_index do |a, i|
+        @email_addresses.each_with_index do |b, j|
+          if i != j && a.present? && a.downcase == b.downcase
+            emails[i].duplicated = true
+          end
+        end
+      end
+    end
+
+    if persisted?
+      emails.each_with_index do |e, i|
+        @email_addresses.each_with_index do |a, j|
+          if i != j && e.address.present? && e.address.downcase == a.downcase
+            emails[i].exchanging = true
+          end
+        end
+      end
+    end
+  end
+
   validates :gender, inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
     after: Date.new(1900, 1, 1),
